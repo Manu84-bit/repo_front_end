865b673fc9c24c390077dc85e472eb0be1a49acb
Manu84-bit <megaskatos@gmail.com>
2021-09-18 23:07:52 -0500

    Chrome extension finished, current tab an input can be saved to local storage

--- Chrome_Ext/scripts/extension.js
+++ Chrome_Ext/scripts/extension.js
@@ -1,43 +1,68 @@
 // chrome://extensions/
+
+
 let myLeads = []
 
-// JSON methos to parse an convert to string objects like arrays
+// JSON methods to parse an convert to string objects like arrays
 // myLeads = `["manu84.com"]`
 // myLeads = JSON.parse(myLeads)
 // myLeads.push("manuAmado.com")
 
 // myLeads = JSON.stringify(myLeads)
 // console.log(myLeads)
-// leadsFromLS = JSON.parse(localStorage.getItem("myLeads"))  localStorage is a property that stores data persistently across sessions
-
 
 const inputField = document.getElementById("input-el") //const is used to avoid reasigned the value, it is a constant.
 const saveBtn = document.getElementById("input-btn")
+const tabBtn = document.getElementById("tab-btn")
+const deleteBtn = document.getElementById("delete-btn")
 const leadsUl = document.getElementById("leadsUl")
+const leadsFromLocalStore = JSON.parse(localStorage.getItem("myLeads"))  //localStorage is a property that stores data persistently across sessions
 
 
-saveBtn.addEventListener("click", function (){  // this way is not necessary to add onclick attribute in the HTML
-    console.log("Button listened")  //addEventlistner is a method that has as parameters a event and a function.
-    myLeads.push (inputField.value) //value is the variable that store the text introduced in the input filed.
-    inputField.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
-})
+if(leadsFromLocalStore){
+    myLeads = leadsFromLocalStore
+    renderLeads(myLeads)
+}
 
 
+saveBtn.addEventListener("click", saveLeads) // this way is not necessary to add onclick attribute in the HTML button
+tabBtn.addEventListener("click", saveTab)
+deleteBtn.addEventListener("dblclick", deleteLeads)
 
-function renderLeads(){
+
+function renderLeads(jsonArr){
     let listItems = ""
-    for (let i=0; i < myLeads.length; i++) {
+    for (let i=0; i < jsonArr.length; i++) {
         //innerHTML allows to add html elements inside others
         // listItems+= "<li><a href='http://"+ myLeads[i]+"' target= '_blank'</a>" + myLeads[i] +"</li>"
         // with templete Strings or litterals `` (command: 'alt gr + }' ) it is possible to embbed the varibles in
         // ${} string an write html styled code.
         listItems+= `<li>
-                       <a href='http://${myLeads[i]}' target='_blank'>${myLeads[i]}</a>
+                       <a href='http://${jsonArr[i]}' target='_blank'>${jsonArr[i]}</a>
                     </li>`
-
     }
-        leadsUl.innerHTML = listItems  //DOM manipultation has a cost, so it is better to asign inner items outside a loop
-    
+        leadsUl.innerHTML = listItems  //DOM manipultation has a cost, so it is better to asign inner items outside a loop    
+}
+
+function saveLeads() {
+    myLeads.push (inputField.value) //value is the variable that store the text introduced in the input filed.
+    inputField.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    renderLeads(myLeads)
+}
+
+function saveTab() {
+    chrome.tabs.query({active:true, currentWindow:true}, function(tabs){ // query to get the current tab url
+    myLeads.push(tabs[0].url) 
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    renderLeads(myLeads)
+   })
+
+}
+
+
+function deleteLeads() {
+    localStorage.clear()
+    myLeads =[]
+    renderLeads(myLeads)
 }
